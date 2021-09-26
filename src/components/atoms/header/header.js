2d6e80053c3652ed7609c76cc236dadb5c4ca5f9
Vaskar Sarma <vaskar.sarma@emirates.com>
2021-09-26 12:05:18 +0400

    update header content

--- src/components/atoms/header/header.js
+++ src/components/atoms/header/header.js
@@ -6,6 +6,7 @@ const Header = () => {
       <h1 className='Center-align-text'>Flight Search Page</h1>
       <h2 hidden={true} className='Center-align-text'>Displaying 10 Flight Routes</h2>
       <h3 hidden={true} className='Center-align-text'>Please Select Any</h3>
+      <p className='Center-align-text'>Found 10 Flights</p>
     </header>
   );
 };
